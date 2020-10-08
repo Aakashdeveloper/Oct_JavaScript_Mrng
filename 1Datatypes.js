0c5749768644dcaae83898cac68c99acbf4102b3
aakashdeveloper <ahanda205@gmail.com>
2020-10-08 07:44:54 +0530

    a

---
+++ 1Datatypes.js
@@ -0,0 +1,22 @@
+DataTypes
+> String "gjtyjy" '45546' "4554" 'ttg' 'hb556' "true" 'false'
+> Number 656 6764687658 67.658 .867
+> Boolean true/false
+
+//Es5 Ecma Script
+var a = "Hi"
+var b = 10
+var c = true
+undefined
+a
+"Hi"
+typeof(a)
+"string"
+typeof(b)
+"number"
+typeof(c)
+"boolean"
+var d = "4545"
+undefined
+typeof(d)
+"string"
