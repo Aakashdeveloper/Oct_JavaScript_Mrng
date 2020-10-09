c29a91aa189bbeabf76c4c4eb8fc4571d5b477cd
aakashdeveloper <ahanda205@gmail.com>
2020-10-09 07:36:20 +0530

    a

---
+++ 2operations.js
@@ -0,0 +1,155 @@
+var a = 10;
+var b = 20;
+
+a+b (sum/addition)
+30
+a-b
+-10
+a*b
+200
+a/b
+0.5
+4%2
+0
+4%3
+1
+4%4
+0
+
+
+var a = "Hi"
+var b = " JavaScript"
+undefined
+a+b
+"Hi JavaScript" (concat)
+a-b
+NaN (not a number)
+a*b
+NaN
+a/b
+NaN
+
+var a = "hi"
+undefined
+var b = 10
+undefined
+a+b
+"hi10"
+a-b
+NaN
+
+
+string +string = string
+string+number = string
+number+string = string
+number+number = number
+
+"10"+20+"30"
+"10"+20+30
+10+20+"30"
+
+
+"10"+20+"30"
+"1020"+"30"
+"102030"
+
+"10"+20+30
+"1020"+30
+"102030"
+
+10+20+"30"
+30+"30"
+"3030"
+
+
+"10"+20+"30"-1
+102029
+"10"+20+"30"-1
+102029
+10+20+"30"-1
+3029
+
+"hi"-1
+NaN
+"10"-1
+9
+"102"-"10"
+92
+"102"*"10"
+1020
+"102"/"10"
+10.2
+"10h"-1
+NaN
+"101"+"10"
+"10110"
+
+//es5
+var a = "101"
+var b = "10"
+parseInt(a)+parseInt(b)
+111
+var c = "hi"
+undefined
+parseInt(c)
+NaN
+
+var d = "10.45"
+var e = "20.11"
+
+var d = "10.45"
+var e = "20.11"
+undefined
+parseInt(d)+parseInt(e)
+30
+parseFloat(d)+parseFloat(e)
+30.56
+var a = "10"
+undefined
+parseFloat(a)
+10
+
+//Es6
+Number(d)+Number(e)
+30.56
+Number(a)
+10
+
+> Number to String
+var a = 10
+undefined
+a.toString()
+"10"
+""+a
+"10"
+
+/////////
+true 
+false
+
+true+true
+
+Issues detected. The new Issues tab displays information about deprecations, breaking changes and other potential problems.Go to IssuesLearn more
+true+true
+2
+true+false
+1
+true-true
+0
+true+10
+11
+true+"hi"
+"truehi"
+
+
+
+true is equal to 1 
+false is equal to 0
+
+
+var a = "true"
+undefined
+var b = "false"
+undefined
+a+b
+"truefalse"
